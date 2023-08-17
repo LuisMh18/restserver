fe88ee87b4a273c1fc26fca72d3f7dda9a4834cb
Luis Ángel Herrera <luis_mh@outlook.es>
2023-08-17 04:31:47 -0600

    Se agrega función de busquedas

--- models/server.js
+++ models/server.js
@@ -9,6 +9,7 @@ class Server {
 
         this.paths = {
             auth: '/api/auth',
+            buscar: '/api/buscar',
             categorias: '/api/categorias',
             productos: '/api/productos',
             usuarios: '/api/usuarios',
@@ -41,12 +42,13 @@ class Server {
 
     routes(){
         this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
     }
 
-    listen(){
+    listen(){ 
         this.app.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port);    
         });
