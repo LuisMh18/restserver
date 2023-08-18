d86d2e7ad2807063dc71857034679e86d7731c22
Luis Ángel Herrera <luis_mh@outlook.es>
2023-08-18 12:20:39 -0600

    Actualización de arhivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,17 +1,12 @@
 const { response, request } = require("express");
+const path = require('path');
+const fs = require('fs');
 const { subirArchivo } = require("../helpers/subir-archivo");
+const { Usuario, Producto } = require('../models');
 
 const cargarArchivo = async(req = request, res = response) => {
 
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-        res.status(400).json({
-            msg: 'No hay archivos que subir'
-        });
-        return;
-    }
-
     try {
-
         // const nombre = await subirArchivo(req.files, ["txt", "md"], 'textos');
         const nombre = await subirArchivo(req.files, undefined, 'imgs');
         res.status(200).json({
@@ -27,10 +22,49 @@ const actualizarImagen = async(req = request, res = response) => {
 
    const { id, coleccion } = req.params;
 
-   res.status(200).json({
-     id, 
-     coleccion
-   });
+   let modelo;
+
+   switch (coleccion) {
+    case 'usuarios':
+        modelo = await Usuario.findById(id);
+        if(!modelo){
+            return res.status(400).json({
+                msg: `No existe un usuario con el id ${id}`
+            });
+        }
+    break;
+
+    case 'productos':
+        modelo = await Producto.findById(id); 
+        if(!modelo){
+            return res.status(400).json({
+                msg: `No existe un producto con el id ${id}`
+            });
+        }
+        
+        break;
+   
+    default:
+        return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+   }
+
+   // Limpiar imágenes previas
+   if(modelo.img){
+     //si existe borramos la imagen del servidor
+     const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img);
+
+     // ! comprobamos si existe el archivo - si existe lo borramos
+     if( fs.existsSync(pathImagen) ){
+        fs.unlinkSync( pathImagen );
+     }
+   }
+
+
+   const nombre = await subirArchivo(req.files, undefined, coleccion);
+   modelo.img = nombre;
+   await modelo.save();
+    
+   res.status(200).json(modelo);
 
 }
 
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
-const { validarCampos } = require('../middlewares');
+const { validarCampos, validarArchivo } = require('../middlewares');
 const { coleccionesPermitidas } = require('../helpers');
 const router = Router();
 
 
-router.post('/', cargarArchivo );
+router.post('/', [ validarArchivo, validarCampos ], cargarArchivo );
+
 router.put('/:coleccion/:id',[
+    validarArchivo,
     check('id', 'No es un ID válido').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'] ) ),
     validarCampos
