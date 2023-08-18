f1ce415f43759a2365981b384fec55a22b56fc40
Luis Ángel Herrera <luis_mh@outlook.es>
2023-08-18 07:50:42 -0600

    Se agrega helper para subida de archivos y se hacen sus respectivas validaciones

---
+++ controllers/uploads.js
@@ -0,0 +1,29 @@
+const { response, request } = require("express");
+const { subirArchivo } = require("../helpers/subir-archivo");
+
+const cargarArchivo = async(req = request, res = response) => {
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+        res.status(400).json({
+            msg: 'No hay archivos que subir'
+        });
+        return;
+    }
+
+    try {
+
+        // const nombre = await subirArchivo(req.files, ["txt", "md"], 'textos');
+        const nombre = await subirArchivo(req.files, undefined, 'imgs');
+        res.status(200).json({
+            nombre
+        });
+
+    } catch(msg){
+        res.status(400).json({ msg });
+    }
+}
+
+
+module.exports = {
+    cargarArchivo 
+}
---
+++ uploads/README.md
@@ -0,0 +1,2 @@
+# Notas #####################
+- Aqui se van a subir todas los archivos
